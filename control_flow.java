3c61f734daf5c654da6ea44d440fbd64fcdfd565
Anate Muhammed <anatemuhammed@gmail.com>
2024-09-09 22:37:35 +0100

    first commit

---
+++ control_flow.java
@@ -0,0 +1,19 @@
+public class control_flow {
+    public static void main(String[] args){
+        int num1 =30;
+        int num2 =12;
+        if (num1==30 && num2<=10){
+            System.out.println("The Value is greaterthan 30");
+            System.out.println("Good Result keep it up!!");
+
+        }
+        else if (num1 <30) {
+            System.out.print("Sorry!!");
+            System.out.print("yOU have to try your best next Time");
+        }
+        else {
+            System.out.println("Remeber this is your Final year! ");
+            System.out.println("You Have to try your very best the for this time");
+        }
+    }
+}
